890b74bf6e7e80ff2e4366652f3ddb32d89ec640
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-03-01 13:26:25 -0800

    GET seed data

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'wash car',
+    completed: false,
+  },
+  {
+    id: 2,
+    todo: 'walk dog',
+    completed: false,
+  },
+  {
+    id: 3,
+    todo: 'take out trash',
+    completed: false,
+  }
+];
