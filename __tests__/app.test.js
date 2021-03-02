decb8c6ac9e9a607a8640a593fb3dc0584e92b8c
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-03-01 16:34:06 -0800

    Tests Passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,57 +9,84 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token; // eslint-disable-line
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    const todo = {
+      todo: 'get groceries',
+      completed: false,
+    };
 
-      const expectation = [
-        {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
-        },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
-        }
-      ];
+    const dbTodo = {
+      ...todo,
+      id: 4,
+      owner_id: 2,
+    };
 
+
+    // POST //
+    test('create a todo', async () => {
       const data = await fakeRequest(app)
-        .get('/animals')
+        .post('/api/todos')
+        .send(todo)
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
         .expect(200);
 
-      expect(data.body).toEqual(expectation);
+      expect(data.body).toEqual([dbTodo]);
+    });
+
+
+    // GET //
+    test('returns all todos for a single user', async () => {
+      const data = await fakeRequest(app)
+        .get('/api/todos')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual([dbTodo]);
+    });
+
+
+    // PUT //
+    test('updates a todo to completed for user', async () => {
+
+      const completedTodo = {
+        id: 4,
+        todo: 'get groceries',
+        completed: true,
+        owner_id: 2,
+      };
+
+      const updatedTodo = await fakeRequest(app)
+        .put('/api/todos/4')
+        .send(dbTodo)
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(updatedTodo.body).toEqual([completedTodo]);
     });
   });
 });
+
